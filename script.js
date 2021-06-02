621a1bfa785fb0dfcf0f297766386c242ffe5149
Eugene <borisenkoe@ukr.net>
2021-06-02 20:18:33 +0300

    rings

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
 const videoElement = document.getElementsByClassName("input_video")[0];
-const canvasElement = document.getElementsByClassName("output_canvas")[0];
-const canvasCtx = canvasElement.getContext("2d");
 
 videoElement.style.cssText =
   "-moz-transform: scale(-1, 1); \
 -webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
 transform: scale(-1, 1); filter: FlipH;";
 
-let jewelryImage = new Image();
+const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const canvasCtx = canvasElement.getContext("2d");
+
+const jewelryImage = new Image();
+// jewelryImage.src =
+//   "https://media-pipe.s3.amazonaws.com/rings/wo_background_resized.png";
 
 const imagesList = document.querySelector("#imageList");
-
 const imageInput = document.querySelector("#imageInput");
-
 let selectedImageId = "";
-
 const images = [];
-
-const canvasWidth = canvasElement.width;
-const canvasHeight = canvasElement.height;
-// const canvasData = canvasCtx.getImageData(0, 0, canvasWidth, canvasHeight);
-
-// jewelryImageLeft.src =
-//   "https://media-pipe.s3.amazonaws.com/earrings/a4fea16e-3ffb-462a-a71f-6d6e7b03e4e3-mask_left.png";
-
-// jewelryImageRight.src =
-//   "https://media-pipe.s3.amazonaws.com/earrings/a4fea16e-3ffb-462a-a71f-6d6e7b03e4e3-mask_right.png";
-
-// jewelryImageRight.src =
-//   "https://media-pipe.s3.amazonaws.com/earrings/a4fea16e-3ffb-462a-a71f-6d6e7b03e4e3-mask_left.png";
-
-// jewelryImageLeft.src =
-//   "https://media-pipe.s3.amazonaws.com/earrings/9034ef50-ee6b-4e82-a8cd-8a756fad99a2-mask_left.png";
-
-// jewelryImageRight.src =
-//   "https://media-pipe.s3.amazonaws.com/earrings/9034ef50-ee6b-4e82-a8cd-8a756fad99a2-mask_right.png";
 
 function updateSelectOptions(data) {
   imagesList.textContent = "";
@@ -48,7 +29,6 @@ function updateSelectOptions(data) {
 
     imagesList.append(option);
   });
-  updateSelectOptions;
 }
 
 function onFileSelected(event) {
@@ -70,136 +50,61 @@ function onFileSelected(event) {
 
 imageInput.addEventListener("change", onFileSelected);
 
-//Aspect ratio
+const exponentialSmoothing = 0.3; // 0.95 for Mozilla, 0.6 for Chrome, depends on web cam framerate
+// const exponentialSmoothingZ = 0.9; //Chrome
+const tooCloseRatio = 0.4;
+const tooFarRatio = 0.05;
+
+//Drawing conditions
+const ratio517 = 0.15;
+const ratioMCPPIPZ = 0.15;
+
+let i = 0;
+let fingerMCPX = [];
+let fingerPIPX = [];
+let fingerMCPY = [];
+let fingerPIPY = [];
+let fingerMCPZ = [];
+let fingerPIPZ = [];
+
+let landmarks5Z = [];
+let landmarks17Z = [];
+
+let ringFingerCenterCoef = 0.5;
+let ringFingerJewelryWidthCoef = 0.35;
+
 let aspectRatio;
 jewelryImage.addEventListener("load", () => {
   aspectRatio = jewelryImage.naturalHeight / jewelryImage.naturalWidth;
 });
 
-let exponentialSmoothing = 0.5;
+let dropDownList = document.getElementById("myList");
+let FINGER;
 
-const jewelryWidthRatio = 0.1;
-
-const finalVideoRatio = 1;
-
-const elongationX = 0.7;
-const elongationY = 1;
-
-let i = 0;
-let totalCounts = 0;
-
-let poseLandmarks93SmoothedX = [];
-let poseLandmarks93SmoothedY = [];
-let poseLandmarks137SmoothedX = [];
-let poseLandmarks137SmoothedY = [];
-let poseLandmarks123SmoothedX = [];
-let poseLandmarks123SmoothedY = [];
-let poseLandmarks352SmoothedX = [];
-let poseLandmarks352SmoothedY = [];
-let poseLandmarks366SmoothedX = [];
-let poseLandmarks366SmoothedY = [];
-let poseLandmarks323SmoothedX = [];
-let poseLandmarks323SmoothedY = [];
-
-function drawPoint(x, y, label, color, size) {
-  if (color == null) {
-    color = "#000";
-  }
-
-  if (size == null) {
-    size = 5;
-  }
-
-  let radius = 0.5 * size;
-
-  // to increase smoothing for numbers with decimal part
-  let pointX = Math.round(x - radius);
-  let pointY = Math.round(y - radius);
-
-  canvasCtx.beginPath();
-  canvasCtx.fillStyle = color;
-  canvasCtx.fillRect(pointX, pointY, size, size);
-  canvasCtx.fill();
-
-  if (label) {
-    let textX = Math.round(x);
-    let textY = Math.round(pointY - 5);
-
-    canvasCtx.font = "Italic 14px Arial";
-    canvasCtx.fillStyle = color;
-    canvasCtx.textAlign = "center";
-    canvasCtx.fillText(label, textX, textY);
-  }
-}
-
-function drawEarring(
-  outterLandMarkX,
-  middleLandmarkX,
-  innerLandMarkX,
-  outterLandMarkY,
-  middleLandmarkY,
-  innerLandMarkY,
-  jewelryImage,
-  side
-) {
-  let drawCondition;
-  if (side === "left") {
-    drawCondition = innerLandMarkX > outterLandMarkX;
-  } else if (side === "right") {
-    drawCondition = outterLandMarkX > innerLandMarkX;
-  }
-
-  if (drawCondition) {
-    let CenterX =
-      outterLandMarkX + (outterLandMarkX - middleLandmarkX) * elongationX;
-
-    let dy;
-
-    if (middleLandmarkY - outterLandMarkY > 0) {
-      dy = outterLandMarkY - (middleLandmarkY - outterLandMarkY) * elongationY;
-    } else {
-      dy = outterLandMarkY + (outterLandMarkY - innerLandMarkY) * elongationY;
-    }
-
-    let dx = CenterX - JewelryWidth / 2;
-
-    console.log();
-
-    canvasCtx.drawImage(
-      jewelryImage,
-      dx * canvasElement.width,
-      dy * canvasElement.height,
-      JewelryWidth * canvasElement.width,
-      JewelryHeight * canvasElement.width
-    );
-  }
-}
-
-//Draw keypoints
-function drawKeyPoint(num, color = "orange", radius = 5) {
-        drawPoint(
-          landmarks[num].x * videoElement.videoWidth * finalVideoRatio,
-          landmarks[num].y * videoElement.videoHeight * finalVideoRatio,
-          String(num),
-          color,
-          radius
-        );
-      }
-
-function smoothArray(smoothedArray, previousValue, currentValue) {
-  return smoothedArray.push(
+function smoothArray(previousValue, currentValue) {
+  return (
     previousValue * exponentialSmoothing +
-      currentValue * (1 - exponentialSmoothing)
+    currentValue * (1 - exponentialSmoothing)
   );
 }
 
+function pointsDistance(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+const spinner = document.querySelector(".loading");
+spinner.ontransitionend = () => {
+  spinner.style.display = "none";
+};
+
 function onResults(results) {
+  // Hide the spinner.
   document.body.classList.add("loaded");
 
   canvasCtx.save();
 
-  canvasElement.width = videoElement.videoWidth * finalVideoRatio;
-  canvasElement.height = videoElement.videoHeight * finalVideoRatio;
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
 
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
   canvasCtx.drawImage(
@@ -210,7 +115,7 @@ function onResults(results) {
     canvasElement.height
   );
 
-  if (results.multiFaceLandmarks) {
+  if (results.multiHandLandmarks && results.multiHandedness) {
     const option = imagesList.options[imagesList.selectedIndex];
     const imageId = option && option.dataset.id;
     selectedImageId = imageId;
@@ -218,196 +123,199 @@ function onResults(results) {
 
     jewelryImage.src = image.src || "";
 
-    for (const landmarks of results?.multiFaceLandmarks) {
-      //drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {
-      //  color: "#C0C0C070",
-      //  lineWidth: 1,
-      //});
-      //   drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {
-      //     color: "#FF3030",
+    for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+      //// Draw connectors
+      //const classification = results.multiHandedness[index];
+      // const isRightHand = classification.label === "Right";
+      // const landmarks = results.multiHandLandmarks[index];
+      // drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {
+      //   color: isRightHand ? "#00FF00" : "#FF0000",
+      // }),
+      //   drawLandmarks(canvasCtx, landmarks, {
+      //     color: isRightHand ? "#00FF00" : "#FF0000",
+      //     fillColor: isRightHand ? "#FF0000" : "#00FF00",
+      //     radius: (x) => {
+      //       return lerp(x.from.z, -0.15, 0.1, 10, 1);
+      //     },
       //   });
-      //   drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, {
-      //     color: "#FF3030",
-      //   });
-      //   drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, {
-      //     color: "#30FF30",
-      //   });
-      //   drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, {
-      //     color: "#30FF30",
-      //   });
-      //   drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {
-      //     color: "#E0E0E0",
-      //   });
-      //   drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {
-      //     color: "#E0E0E0",
-      //   });
-
-      if (i == 0) {
-        poseLandmarks93SmoothedX.push(landmarks[93].x);
-        poseLandmarks93SmoothedY.push(landmarks[93].y);
-        poseLandmarks137SmoothedX.push(landmarks[137].x);
-        poseLandmarks137SmoothedY.push(landmarks[137].y);
-        poseLandmarks123SmoothedX.push(landmarks[123].x);
-        poseLandmarks123SmoothedY.push(landmarks[123].y);
-        poseLandmarks352SmoothedX.push(landmarks[352].x);
-        poseLandmarks352SmoothedY.push(landmarks[352].y);
-        poseLandmarks366SmoothedX.push(landmarks[366].x);
-        poseLandmarks366SmoothedY.push(landmarks[366].y);
-        poseLandmarks323SmoothedX.push(landmarks[323].x);
-        poseLandmarks323SmoothedY.push(landmarks[323].y);
-      } else {
-        smoothArray(
-          poseLandmarks93SmoothedX,
-          poseLandmarks93SmoothedX[i - 1],
-          landmarks[93].x
-        );
-
-        smoothArray(
-          poseLandmarks93SmoothedY,
-          poseLandmarks93SmoothedY[i - 1],
-          landmarks[93].y
-        );
-
-        smoothArray(
-          poseLandmarks137SmoothedX,
-          poseLandmarks137SmoothedX[i - 1],
-          landmarks[137].x
-        );
-
-        smoothArray(
-          poseLandmarks137SmoothedY,
-          poseLandmarks137SmoothedY[i - 1],
-          landmarks[137].y
-        );
-
-        smoothArray(
-          poseLandmarks123SmoothedX,
-          poseLandmarks123SmoothedX[i - 1],
-          landmarks[123].x
-        );
-
-        smoothArray(
-          poseLandmarks123SmoothedY,
-          poseLandmarks123SmoothedY[i - 1],
-          landmarks[123].y
-        );
-
-        smoothArray(
-          poseLandmarks352SmoothedX,
-          poseLandmarks352SmoothedX[i - 1],
-          landmarks[352].x
-        );
-
-        smoothArray(
-          poseLandmarks352SmoothedY,
-          poseLandmarks352SmoothedY[i - 1],
-          landmarks[352].y
-        );
-
-        smoothArray(
-          poseLandmarks366SmoothedX,
-          poseLandmarks366SmoothedX[i - 1],
-          landmarks[366].x
-        );
-
-        smoothArray(
-          poseLandmarks366SmoothedY,
-          poseLandmarks366SmoothedY[i - 1],
-          landmarks[366].y
-        );
-
-        smoothArray(
-          poseLandmarks323SmoothedX,
-          poseLandmarks323SmoothedX[i - 1],
-          landmarks[323].x
-        );
-
-        smoothArray(
-          poseLandmarks323SmoothedY,
-          poseLandmarks323SmoothedY[i - 1],
-          landmarks[323].y
-        );
-      }
-
-      JewelryWidth =
-        (Math.max(
-          poseLandmarks352SmoothedX[i],
-          poseLandmarks366SmoothedX[i],
-          poseLandmarks323SmoothedX[i]
-        ) -
-          Math.min(
-            poseLandmarks123SmoothedX[i],
-            poseLandmarks137SmoothedX[i],
-            poseLandmarks93SmoothedX[i]
-          )) *
-        jewelryWidthRatio;
-      JewelryHeight = JewelryWidth * aspectRatio;
-
-      drawEarring(
-        poseLandmarks93SmoothedX[i],
-        poseLandmarks137SmoothedX[i],
-        poseLandmarks123SmoothedX[i],
-        poseLandmarks93SmoothedY[i],
-        poseLandmarks137SmoothedY[i],
-        poseLandmarks123SmoothedY[i],
-        jewelryImage,
-        "left"
-      );
-
-      drawEarring(
-        poseLandmarks323SmoothedX[i],
-        poseLandmarks366SmoothedX[i],
-        poseLandmarks352SmoothedX[i],
-        poseLandmarks323SmoothedY[i],
-        poseLandmarks366SmoothedY[i],
-        poseLandmarks352SmoothedY[i],
-        jewelryImage,
-        "right"
-      );
-
-      //drawKeyPoint(93);
-      //drawKeyPoint(137);
-      //drawKeyPoint(323);
-      //drawKeyPoint(366);
-      //drawKeyPoint(123);
-      //drawKeyPoint(352);
     }
+
+    FINGER = dropDownList.options[dropDownList.selectedIndex].text;
+
+    switch (FINGER) {
+      case "pinky":
+        mcpLandmark = 17;
+        pipLandmark = 18;
+        break;
+      case "ring finger":
+        mcpLandmark = 13;
+        pipLandmark = 14;
+        break;
+      case "middle finger":
+        mcpLandmark = 9;
+        pipLandmark = 10;
+        break;
+      case "index finger":
+        mcpLandmark = 5;
+        pipLandmark = 6;
+        break;
+    }
+
+    console.log(FINGER);
+
+    console.log(results.multiHandedness[0].label);
+
+    let landmarks = results.multiHandLandmarks[0];
+
+    //SMOOTHED COORDINATES
+    let smoothedMCPX = smoothArray(fingerMCPX[i - 1], landmarks[mcpLandmark].x);
+
+    let smoothedPIPX = smoothArray(fingerPIPX[i - 1], landmarks[pipLandmark].x);
+
+    let smoothedMCPY = smoothArray(fingerMCPY[i - 1], landmarks[mcpLandmark].y);
+
+    let smoothedPIPY = smoothArray(fingerPIPY[i - 1], landmarks[pipLandmark].y);
+
+    let smoothedMCPZ = smoothArray(fingerMCPZ[i - 1], landmarks[mcpLandmark].z);
+
+    let smoothedPIPZ = smoothArray(fingerPIPZ[i - 1], landmarks[pipLandmark].z);
+
+    let smoothed5Z;
+
+    let smoothed17Z;
+
+    if (FINGER === "indexFinger") {
+      smoothed5Z = smoothedMCPZ;
+    } else {
+      smoothed5Z = smoothArray(landmarks5Z[i - 1], landmarks[5].z);
+    }
+
+    if (FINGER === "pinky") {
+      smoothed17Z = smoothedMCPZ;
+    } else {
+      smoothed17Z = smoothArray(landmarks17Z[i - 1], landmarks[17].z);
+    }
+
+    //DRAWING CONDITIONS
+    if (Math.abs(smoothed5Z - smoothed17Z) > ratio517) {
+      console.log("Palm should be perpendicular to the camera");
+      return;
+    }
+
+    if (results.multiHandedness[0].label === "Right") {
+      if (landmarks[17].x > landmarks[5].x) {
+        console.log("The outside of the palm shoud be facing the camera");
+        return;
+      }
+    }
+
+    if (results.multiHandedness[0].label === "Left") {
+      if (landmarks[5].x > landmarks[17].x) {
+        console.log("The outside of the palm shoud be facing the camera");
+        return;
+      }
+    }
+
+    let distance517 = pointsDistance(
+      landmarks[5].x,
+      landmarks[5].y,
+      landmarks[17].x,
+      landmarks[17].y
+    );
+
+    if (distance517 > tooCloseRatio) {
+      console.log("Palm is too close to  the camera");
+      return;
+    }
+
+    if (distance517 < tooFarRatio) {
+      console.log("Palm is too far from the camera");
+      return;
+    }
+
+    if (Math.abs(smoothedMCPZ - smoothedPIPZ) > ratioMCPPIPZ) {
+      console.log("please straighten your fingers");
+      return;
+    }
+
+    if (i === 0) {
+      //PUSHING TO ARRAYS
+      fingerMCPX.push(landmarks[mcpLandmark].x);
+      fingerPIPX.push(landmarks[pipLandmark].x);
+      fingerMCPY.push(landmarks[mcpLandmark].y);
+      fingerPIPY.push(landmarks[pipLandmark].y);
+      fingerMCPZ.push(landmarks[mcpLandmark].z);
+      fingerPIPZ.push(landmarks[pipLandmark].z);
+      landmarks5Z.push(landmarks[5].z);
+      landmarks17Z.push(landmarks[17].z);
+    } else {
+      fingerMCPX.push(smoothedMCPX);
+      fingerPIPX.push(smoothedPIPX);
+      fingerMCPY.push(smoothedMCPY);
+      fingerPIPY.push(smoothedPIPY);
+      fingerMCPZ.push(smoothedMCPZ);
+      fingerPIPZ.push(smoothedPIPZ);
+      landmarks5Z.push(smoothed5Z);
+      landmarks17Z.push(smoothed17Z);
+    }
+
+    let angle = -Math.atan(
+      (smoothedPIPX - smoothedMCPX) / (smoothedPIPY - smoothedMCPY)
+    );
+
+    let mcpipDistance = pointsDistance(
+      smoothedMCPX,
+      smoothedMCPY,
+      smoothedPIPX,
+      smoothedPIPY
+    );
+    let width =
+      mcpipDistance * canvasElement.width * ringFingerJewelryWidthCoef;
+    let height = aspectRatio * width;
+
+    let centerX =
+      (smoothedMCPX + (smoothedPIPX - smoothedMCPX) * ringFingerCenterCoef) *
+      canvasElement.width;
+    let centerY =
+      (smoothedMCPY + (smoothedPIPY - smoothedMCPY) * ringFingerCenterCoef) *
+      canvasElement.height;
+
+    canvasCtx.translate(centerX, centerY);
+
+    canvasCtx.rotate(angle);
+
+    canvasCtx.translate(-centerX, -centerY);
+
+    canvasCtx.drawImage(
+      jewelryImage,
+      centerX - width / 2,
+      centerY - height / 2,
+      width,
+      height
+    );
+
+    canvasCtx.restore();
     i++;
     console.log(i);
   }
-  canvasCtx.restore();
-
-  totalCounts++;
-  console.log(totalCounts);
-  let currentTime = Date.now();
-
-  let FPS = (totalCounts / (currentTime - startTime)) * 1000; //time in miliseconds
-  console.log("FPS", FPS);
-
-  exponentialSmoothing = 0.01 * FPS + 0.16;
-  console.log("exponentialSmoothing", exponentialSmoothing);
 }
 
-const faceMesh = new FaceMesh({
+const hands = new Hands({
   locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@0.3/${file}`;
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.3/${file}`;
   },
 });
-faceMesh.setOptions({
+hands.setOptions({
   selfieMode: true,
-  maxNumFaces: 1,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5,
+  maxNumHands: 1,
+  minDetectionConfidence: 0.8,
+  minTrackingConfidence: 0.8,
 });
-faceMesh.onResults(onResults);
+hands.onResults(onResults);
 
 const camera = new Camera(videoElement, {
   onFrame: async () => {
-    await faceMesh.send({ image: videoElement });
+    await hands.send({ image: videoElement });
   },
-  // width: canvasWidth,
-  // height: canvasHeight,
 });
-
-startTime = Date.now();
 camera.start();
