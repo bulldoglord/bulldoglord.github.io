a0dd584a5bb29e8b1b2d7d646f0d9a8b9dda5575
Eugene <borisenkoe@ukr.net>
2021-06-09 15:12:35 +0300

    iOS check

--- index.js
+++ index.js
@@ -4,26 +4,60 @@
  
  const canvasElement = document.getElementById('output');
  const canvasCtx = canvasElement.getContext("2d");
+ 
+ canvasElement.style.cssText =
+  "-moz-transform: scale(-1, 1); \
+-webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
+transform: scale(-1, 1); filter: FlipH;";
 
- fingerLookupIndices = {
-  thumb: [0, 1, 2, 3, 4],
-  indexFinger: [0, 5, 6, 7, 8],
-  middleFinger: [0, 9, 10, 11, 12],
-  ringFinger: [0, 13, 14, 15, 16],
-  pinky: [0, 17, 18, 19, 20]
-};
+function isMobile() {
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return isAndroid || isiOS;
+}
 
- function drawPoint(y, x, r) {
-   ctx.beginPath();
-   ctx.arc(x, y, r, 0, 2 * Math.PI);
-   ctx.fill();
- }
+ const mobile = isMobile();
  
  const jewelryImage = new Image();
- jewelryImage.src = "images/image-3.png";
+ const imagesList = document.querySelector("#imageList");
+ const imageInput = document.querySelector("#imageInput");
+ let selectedImageId = "";
+ const images = [];
+ 
+ function updateSelectOptions(data) {
+   imagesList.textContent = "";
+ 
+   data.forEach((image) => {
+     const option = document.createElement("option");
+     option.setAttribute("data-id", image.id);
+     option.textContent = image.name;
+ 
+     if (image.id === selectedImageId) option.selected = true;
+ 
+     imagesList.append(option);
+   });
+ }
+ 
+ function onFileSelected(event) {
+   const file = event.target.files[0];
+   const reader = new FileReader();
+ 
+   reader.onloadend = function (e) {
+     const {
+       target: { result },
+     } = e;
+     const { name } = file;
+ 
+     images.push({ id: `${images.length}-${name}`, name, src: result });
+ 
+     updateSelectOptions(images);
+   };
+   reader.readAsDataURL(file);
+ }
+ 
+ imageInput.addEventListener("change", onFileSelected);
 
- let exponentialSmoothing = 0.3; // 0.95 for Mozilla, 0.6 for Chrome, depends on web cam framerate
- // const exponentialSmoothingZ = 0.9; //Chrome
+ let exponentialSmoothing = 0.3;
  const tooCloseRatio = 0.4;
  const tooFarRatio = 0.05;
  
@@ -32,7 +66,6 @@
  const ratioMCPPIPZ = 0.15;
  
  let i = 0;
- let totalCounts = 0;
  
  let dropDownList = document.getElementById("myList");
  let FINGER;
@@ -60,43 +93,13 @@
     previousValue * exponentialSmoothing +
     currentValue * (1 - exponentialSmoothing)
   );
-}
+ }
 
 function pointsDistance(x1, y1, x2, y2) {
   return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
- function drawKeypoints(keypoints) {
-   const keypointsArray = keypoints;
- 
-   for (let i = 0; i < keypointsArray.length; i++) {
-     const y = keypointsArray[i][0];
-     const x = keypointsArray[i][1];
-     drawPoint(x - 2, y - 2, 3);
-   }
- 
-   const fingers = Object.keys(fingerLookupIndices);
-   for (let i = 0; i < fingers.length; i++) {
-     const finger = fingers[i];
-     const points = fingerLookupIndices[finger].map(idx => keypoints[idx]);
-     drawPath(points, false);
-   }
- }
- 
- function drawPath(points, closePath) {
-   const region = new Path2D();
-   region.moveTo(points[0][0], points[0][1]);
-   for (let i = 1; i < points.length; i++) {
-     const point = points[i];
-     region.lineTo(point[0], point[1]);
-   }
- 
-   if (closePath) {
-     region.closePath();
-   }
-   ctx.stroke(region);
- }
- 
+
  let model;
  
  async function setupCamera() {
@@ -112,8 +115,8 @@ function pointsDistance(x1, y1, x2, y2) {
        facingMode: 'user',
        // Only setting the video to a specified size in order to accommodate a
        // point cloud, so on mobile devices accept the default size.
-       width: VIDEO_WIDTH,
-       height: VIDEO_HEIGHT
+       width: undefined,
+       height: undefined
      },
    });
    video.srcObject = stream;
@@ -133,42 +136,40 @@ function pointsDistance(x1, y1, x2, y2) {
 
 
  async function main() {
-
    model = await handpose.load();
    let video;
  
    try {
      video = await loadVideo();
    } catch (e) {
-     let info = document.getElementById('info');
-     info.textContent = e.message;
-     info.style.display = 'block';
+     console.log(e.message);
      throw e;
    }
-
-
- 
-   // These anchor points allow the hand pointcloud to resize according to its
-   // position in the input.
-   ANCHOR_POINTS = [
-     [0, 0, 0], [0, -VIDEO_HEIGHT, 0], [-VIDEO_WIDTH, 0, 0],
-     [-VIDEO_WIDTH, -VIDEO_HEIGHT, 0]
-   ];
    
    landmarksRealTime(video);
  }
  
  const landmarksRealTime = async (video) => {
    async function frameLandmarks() {
+    
     canvasCtx.save();
     canvasElement.width = video.videoWidth;
     canvasElement.height = video.videoHeight;
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     canvasCtx.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    
     const predictions = await model.estimateHands(video);
 
     if (predictions.length > 0) {
       const landmarks = predictions[0].landmarks;
+
+      console.log(predictions[0].annotations);
+
+      const option = imagesList.options[imagesList.selectedIndex];
+      const imageId = option && option.dataset.id;
+      selectedImageId = imageId;
+      const image = images.find((elem) => elem.id === imageId) || {};
+      jewelryImage.src = image.src || "";
 
       FINGER = dropDownList.options[dropDownList.selectedIndex].text;
 
@@ -200,7 +201,7 @@ function pointsDistance(x1, y1, x2, y2) {
       let smoothed5Z;
       let smoothed17Z;
       
-      /*if (FINGER === "indexFinger") {
+      if (FINGER === "indexFinger") {
         smoothed5Z = smoothedMCPZ;
       } else {
         smoothed5Z = smoothArray(landmarks5Z[i - 1], landmarks[5][2]);
@@ -212,48 +213,43 @@ function pointsDistance(x1, y1, x2, y2) {
         smoothed17Z = smoothArray(landmarks17Z[i - 1], landmarks[17][2]);
       }
   
-      //DRAWING CONDITIONS
       if (Math.abs(smoothed5Z - smoothed17Z) > ratio517) {
         console.log("Palm should be perpendicular to the camera");
-        return;
       }
-  
+      /*
       if (results.multiHandedness[0].label === "Right") {
-        if (landmarks[17].x > landmarks[5].x) {
+        if (landmarks[17][0] > landmarks[5][0]) {
           console.log("The outside of the palm shoud be facing the camera");
           return;
         }
       }
   
       if (results.multiHandedness[0].label === "Left") {
-        if (landmarks[5].x > landmarks[17].x) {
+        if (landmarks[5][0] > landmarks[17][0]) {
           console.log("The outside of the palm shoud be facing the camera");
           return;
         }
-      }
+      }*/
       
        let distance517 = pointsDistance(
-        landmarks[5].x,
-        landmarks[5].y,
-        landmarks[17].x,
-        landmarks[17].y
+        landmarks[5][0],
+        landmarks[5][1],
+        landmarks[17][0],
+        landmarks[17][1]
       );
 
       if (distance517 > tooCloseRatio) {
         console.log("Palm is too close to  the camera");
-        return;
       }
 
       if (distance517 < tooFarRatio) {
         console.log("Palm is too far from the camera");
-        return;
       }
 
       if (Math.abs(smoothedMCPZ - smoothedPIPZ) > ratioMCPPIPZ) {
         console.log("please straighten your fingers");
-        return;
       }
-      */
+      
       if (i === 0) {
         fingerMCPX.push(landmarks[mcpLandmark][0]);
         fingerPIPX.push(landmarks[pipLandmark][0]);
@@ -277,43 +273,29 @@ function pointsDistance(x1, y1, x2, y2) {
       let angle = -Math.atan(
         (smoothedPIPX - smoothedMCPX) / (smoothedPIPY - smoothedMCPY)
       );
+
       let mcpipDistance = pointsDistance(
         smoothedMCPX,
         smoothedMCPY,
         smoothedPIPX,
         smoothedPIPY
       );
-      let width =
-      mcpipDistance * ringFingerJewelryWidthCoef;
-    let height = aspectRatio * width;
 
-    let centerX =
-      (smoothedMCPX + (smoothedPIPX - smoothedMCPX) * ringFingerCenterCoef);
-    let centerY =
-      (smoothedMCPY + (smoothedPIPY - smoothedMCPY) * ringFingerCenterCoef);
+      let width = mcpipDistance * ringFingerJewelryWidthCoef;
+      let height = aspectRatio * width;
+      let centerX = (smoothedMCPX + (smoothedPIPX - smoothedMCPX) * ringFingerCenterCoef);
+      let centerY = (smoothedMCPY + (smoothedPIPY - smoothedMCPY) * ringFingerCenterCoef);
 
-    canvasCtx.translate(centerX, centerY);
-
-    canvasCtx.rotate(angle);
-
-    canvasCtx.translate(-centerX, -centerY);
-
-    canvasCtx.drawImage(
-      jewelryImage,
-      centerX - width / 2,
-      centerY - height / 2,
-      width,
-      height
-    );
-    
-    //console.log('Width: ', width);
-    //console.log('Height: ', height);
-
-    canvasCtx.restore();
+      canvasCtx.translate(centerX, centerY);
+      canvasCtx.rotate(angle);
+      canvasCtx.translate(-centerX, -centerY);
+      canvasCtx.drawImage(jewelryImage,centerX - width / 2, centerY - height / 2, width, height);
       
-      
-      //drawKeypoints(landmarks, predictions[0].annotations);
-    i++;
+      //console.log('Width: ', width);
+      //console.log('Height: ', height);
+
+      canvasCtx.restore();
+      i++;
 
     }
      rafID = requestAnimationFrame(frameLandmarks);
