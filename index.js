cbe760e921e5708eb7493db2e3a95f77987e57c3
Eugene <borisenkoe@ukr.net>
2021-06-17 15:51:27 +0300

    rings deskpto folder

--- index.js
+++ index.js
@@ -1,275 +1,310 @@
-const canvasElement = document.getElementById("output");
-const canvasCtx = canvasElement.getContext("2d");
 
-canvasElement.style.cssText =
+ const VIDEO_WIDTH = 640;
+ const VIDEO_HEIGHT = 500;
+ 
+ const canvasElement = document.getElementById('output');
+ const canvasCtx = canvasElement.getContext("2d");
+ 
+ canvasElement.style.cssText =
   "-moz-transform: scale(-1, 1); \
-  -webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
-  transform: scale(-1, 1); filter: FlipH;";
+-webkit-transform: scale(-1, 1); -o-transform: scale(-1, 1); \
+transform: scale(-1, 1); filter: FlipH;";
 
-const jewelryImage = new Image();
-const imagesList = document.querySelector("#imageList");
-const imageInput = document.querySelector("#imageInput");
-const images = [];
-
-imageInput.addEventListener("change", onFileSelected);
-
-let exponentialSmoothing = 0.5;
-const relativeJewelryWidth = 0.35;
-
-let i = 0;
-let totalCounts = 0;
-
-let model;
-
-let leftX = [];
-let rightX = [];
-let leftY = [];
-let rightY = [];
-
-let aspectRatio;
-jewelryImage.addEventListener("load", () => {
-  aspectRatio = jewelryImage.naturalHeight / jewelryImage.naturalWidth;
-});
-
-function updateSelectOptions(data) {
-  imagesList.textContent = "";
-
-  data.forEach((image) => {
-    const option = document.createElement("option");
-    option.setAttribute("data-id", image.id);
-    option.textContent = image.name;
-
-    if (image.id === selectedImageId) option.selected = true;
-
-    imagesList.append(option);
-  });
+function isMobile() {
+  const isAndroid = /Android/i.test(navigator.userAgent);
+  const isiOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
+  return isAndroid || isiOS;
 }
 
-function onFileSelected(event) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+ const mobile = isMobile();
+ 
+ const jewelryImage = new Image();
+ const imagesList = document.querySelector("#imageList");
+ const imageInput = document.querySelector("#imageInput");
+ let selectedImageId = "";
+ const images = [];
+ 
+ function updateSelectOptions(data) {
+   imagesList.textContent = "";
+ 
+   data.forEach((image) => {
+     const option = document.createElement("option");
+     option.setAttribute("data-id", image.id);
+     option.textContent = image.name;
+ 
+     if (image.id === selectedImageId) option.selected = true;
+ 
+     imagesList.append(option);
+   });
+ }
+ 
+ function onFileSelected(event) {
+   const file = event.target.files[0];
+   const reader = new FileReader();
+ 
+   reader.onloadend = function (e) {
+     const {
+       target: { result },
+     } = e;
+     const { name } = file;
+ 
+     images.push({ id: `${images.length}-${name}`, name, src: result });
+ 
+     updateSelectOptions(images);
+   };
+   reader.readAsDataURL(file);
+ }
+ 
+ imageInput.addEventListener("change", onFileSelected);
 
-  reader.onloadend = function (e) {
-    const {
-      target: { result },
-    } = e;
-    const { name } = file;
+ let exponentialSmoothing = 0.3;
+ const tooCloseRatio = 0.4;
+ const tooFarRatio = 0.05;
+ 
+ //Drawing conditions
+ const ratio517 = 0.15;
+ const ratioMCPPIPZ = 0.15;
+ 
+ let i = 0;
+ 
+ let dropDownList = document.getElementById("myList");
+ let FINGER;
 
-    images.unshift({ id: `${images.length}-${name}`, name, src: result });
+ let fingerMCPX = [];
+ let fingerPIPX = [];
+ let fingerMCPY = [];
+ let fingerPIPY = [];
+ let fingerMCPZ = [];
+ let fingerPIPZ = [];
+ 
+ let landmarks5Z = [];
+ let landmarks17Z = [];
+ 
+ let ringFingerCenterCoef = 0.5;
+ let ringFingerJewelryWidthCoef = 0.42;
+ 
+ let aspectRatio;
+ jewelryImage.addEventListener("load", () => {
+   aspectRatio = jewelryImage.naturalHeight / jewelryImage.naturalWidth;
+ });
 
-    updateSelectOptions(images);
-  };
-  reader.readAsDataURL(file);
-}
-
-function smoothArray(smoothedArray, previousValue, currentValue) {
-  return smoothedArray.push(
+ function smoothArray(previousValue, currentValue) {
+  return (
     previousValue * exponentialSmoothing +
-      currentValue * (1 - exponentialSmoothing)
+    currentValue * (1 - exponentialSmoothing)
   );
+ }
+
+function pointsDistance(x1, y1, x2, y2) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
-async function setupCamera() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    throw new Error(
-      "Browser API navigator.mediaDevices.getUserMedia not available"
-    );
-  }
 
-  const video = document.getElementById("video");
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: {
-      facingMode: "user"
-    },
-  });
-  video.srcObject = stream;
+ let model;
+ 
+ async function setupCamera() {
+   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+     throw new Error(
+         'Browser API navigator.mediaDevices.getUserMedia not available');
+   }
+ 
+   const video = document.getElementById('video');
+   const stream = await navigator.mediaDevices.getUserMedia({
+     'audio': false,
+     'video': {
+       facingMode: 'user',
+       // Only setting the video to a specified size in order to accommodate a
+       // point cloud, so on mobile devices accept the default size.
+       width: undefined,
+       height: undefined
+     },
+   });
+   video.srcObject = stream;
+ 
+   return new Promise((resolve) => {
+     video.onloadedmetadata = () => {
+       resolve(video);
+     };
+   });
+ }
+ 
+ async function loadVideo() {
+   const video = await setupCamera();
+   video.play();
+   return video;
+ }
 
-  return new Promise((resolve) => {
-    video.onloadedmetadata = () => {
-      resolve(video);
-    };
-  });
-}
 
-async function loadVideo() {
-  const video = await setupCamera();
-  video.play();
-  return video;
-}
-
-const landmarksRealTime = async (video, detector) => {
-  async function frameLandmarks() {
+ async function main() {
+   model = await handpose.load();
+   let video;
+ 
+   try {
+     video = await loadVideo();
+   } catch (e) {
+     console.log(e.message);
+     throw e;
+   }
+   
+   landmarksRealTime(video);
+ }
+ 
+ const landmarksRealTime = async (video) => {
+   async function frameLandmarks() {
+    
     canvasCtx.save();
+    canvasElement.width = video.videoWidth;
+    canvasElement.height = video.videoHeight;
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
     canvasCtx.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+    
+    const predictions = await model.estimateHands(video);
 
-    const option = imagesList.options[imagesList.selectedIndex];
-    const imageId = option && option.dataset.id;
-    selectedImageId = imageId;
-    const image = images.find((elem) => elem.id === imageId) || {};
-    jewelryImage.src = image.src || "";
+    if (predictions.length > 0) {
+      const landmarks = predictions[0].landmarks;
 
-    // const estimationConfig = {
-    //   maxPoses: 5,
-    //   flipHorizontal: false,
-    //   scoreThreshold: 0.5,
-    //   nmsRadius: 20,
-    // };
-    // const poses = await detector.estimatePoses(video, estimationConfig);
+      console.log(predictions[0].annotations);
 
-    const poses = await detector.estimatePoses(video); //working
+      const option = imagesList.options[imagesList.selectedIndex];
+      const imageId = option && option.dataset.id;
+      selectedImageId = imageId;
+      const image = images.find((elem) => elem.id === imageId) || {};
+      jewelryImage.src = image.src || "";
 
-    if (poses.length > 0) {
-      const landmarks = poses[0].keypoints;
+      FINGER = dropDownList.options[dropDownList.selectedIndex].text;
 
-      // console.log("landmarks", landmarks);
-
-      // // Draw keypoints
-      // for (let i = 9; i < 11; i++) {
-      //   const x = landmarks[i].x;
-      //   const y = landmarks[i].y;
-      //   canvasCtx.beginPath();
-      //   canvasCtx.arc(x, y, 5 /* radius */, 0, 2 * Math.PI);
-      //   canvasCtx.fill();
-      // }
-
-      if (i === 0) {
-        leftX.push(landmarks[5].x);
-        leftY.push(landmarks[5].y);
-        rightX.push(landmarks[6].x);
-        rightY.push(landmarks[6].y);
+      switch (FINGER) {
+        case "pinky":
+          mcpLandmark = 17;
+          pipLandmark = 18;
+          break;
+        case "ring finger":
+          mcpLandmark = 13;
+          pipLandmark = 14;
+          break;
+        case "middle finger":
+          mcpLandmark = 9;
+          pipLandmark = 10;
+          break;
+        case "index finger":
+          mcpLandmark = 5;
+          pipLandmark = 6;
+          break;
+      }
+  
+      let smoothedMCPX = smoothArray(fingerMCPX[i - 1], landmarks[mcpLandmark][0]);
+      let smoothedPIPX = smoothArray(fingerPIPX[i - 1], landmarks[pipLandmark][0]);
+      let smoothedMCPY = smoothArray(fingerMCPY[i - 1], landmarks[mcpLandmark][1]);
+      let smoothedPIPY = smoothArray(fingerPIPY[i - 1], landmarks[pipLandmark][1]);
+      let smoothedMCPZ = smoothArray(fingerMCPZ[i - 1], landmarks[mcpLandmark][2]);
+      let smoothedPIPZ = smoothArray(fingerPIPZ[i - 1], landmarks[pipLandmark][2]);
+      let smoothed5Z;
+      let smoothed17Z;
+      
+      if (FINGER === "indexFinger") {
+        smoothed5Z = smoothedMCPZ;
       } else {
-        smoothArray(leftX, leftX[i - 1], landmarks[5].x);
-        smoothArray(leftY, leftY[i - 1], landmarks[5].y);
-        smoothArray(rightX, rightX[i - 1], landmarks[6].x);
-        smoothArray(rightY, rightY[i - 1], landmarks[6].y);
+        smoothed5Z = smoothArray(landmarks5Z[i - 1], landmarks[5][2]);
+      }
+  
+      if (FINGER === "pinky") {
+        smoothed17Z = smoothedMCPZ;
+      } else {
+        smoothed17Z = smoothArray(landmarks17Z[i - 1], landmarks[17][2]);
+      }
+  
+      if (Math.abs(smoothed5Z - smoothed17Z) > ratio517) {
+        console.log("Palm should be perpendicular to the camera");
+      }
+      /*
+      if (results.multiHandedness[0].label === "Right") {
+        if (landmarks[17][0] > landmarks[5][0]) {
+          console.log("The outside of the palm shoud be facing the camera");
+          return;
+        }
+      }
+  
+      if (results.multiHandedness[0].label === "Left") {
+        if (landmarks[5][0] > landmarks[17][0]) {
+          console.log("The outside of the palm shoud be facing the camera");
+          return;
+        }
+      }*/
+      
+       let distance517 = pointsDistance(
+        landmarks[5][0],
+        landmarks[5][1],
+        landmarks[17][0],
+        landmarks[17][1]
+      );
+
+      if (distance517 > tooCloseRatio) {
+        console.log("Palm is too close to  the camera");
       }
 
-      // //DRAWING CONDITIONS
-      // if (
-      //   (rightX[i] < landmarks[10].x && landmarks[10].y < landmarks[8].y) ||
-      //   (leftX[i] > landmarks[9].x && landmarks[9].y < landmarks[7].y)
-      // ) {
-      //   console.log("Necklace is occluded by hand");
-      //   return;
-      // }
+      if (distance517 < tooFarRatio) {
+        console.log("Palm is too far from the camera");
+      }
 
-      let xCenter = (leftX[i] + rightX[i]) / 2;
-      let yCenter = (leftY[i] + rightY[i]) / 2;
+      if (Math.abs(smoothedMCPZ - smoothedPIPZ) > ratioMCPPIPZ) {
+        console.log("please straighten your fingers");
+      }
+      
+      if (i === 0) {
+        fingerMCPX.push(landmarks[mcpLandmark][0]);
+        fingerPIPX.push(landmarks[pipLandmark][0]);
+        fingerMCPY.push(landmarks[mcpLandmark][1]);
+        fingerPIPY.push(landmarks[pipLandmark][1]);
+        fingerMCPZ.push(landmarks[mcpLandmark][2]);
+        fingerPIPZ.push(landmarks[pipLandmark][2]);
+        landmarks5Z.push(landmarks[5][2]);
+        landmarks17Z.push(landmarks[17][2]);
+      } else {
+        fingerMCPX.push(smoothedMCPX);
+        fingerPIPX.push(smoothedPIPX);
+        fingerMCPY.push(smoothedMCPY);
+        fingerPIPY.push(smoothedPIPY);
+        fingerMCPZ.push(smoothedMCPZ);
+        fingerPIPZ.push(smoothedPIPZ);
+        landmarks5Z.push(smoothed5Z);
+        landmarks17Z.push(smoothed17Z);
+      }
+  
+      let angle = -Math.atan(
+        (smoothedPIPX - smoothedMCPX) / (smoothedPIPY - smoothedMCPY)
+      );
 
-      let xDistanceShoulders = Math.abs(leftX[i] - rightX[i]);
+      let mcpipDistance = pointsDistance(
+        smoothedMCPX,
+        smoothedMCPY,
+        smoothedPIPX,
+        smoothedPIPY
+      );
 
-      let dWidth = xDistanceShoulders * relativeJewelryWidth;
-      let dHeight = dWidth * aspectRatio;
+      let width = mcpipDistance * ringFingerJewelryWidthCoef;
+      let height = aspectRatio * width;
+      let centerX = (smoothedMCPX + (smoothedPIPX - smoothedMCPX) * ringFingerCenterCoef);
+      let centerY = (smoothedMCPY + (smoothedPIPY - smoothedMCPY) * ringFingerCenterCoef);
 
-      let dx = xCenter - (relativeJewelryWidth / 2) * xDistanceShoulders;
-      let dy = yCenter - 0.5 * dHeight;
-
-      canvasCtx.drawImage(jewelryImage, dx, dy, dWidth, dHeight);
+      canvasCtx.translate(centerX, centerY);
+      canvasCtx.rotate(angle);
+      canvasCtx.translate(-centerX, -centerY);
+      canvasCtx.drawImage(jewelryImage,centerX - width / 2, centerY - height / 2, width, height);
+      
+      //console.log('Width: ', width);
+      //console.log('Height: ', height);
 
       canvasCtx.restore();
       i++;
-      console.log("i", i);
-      totalCounts++;
-      console.log(totalCounts);
-      let currentTime = Date.now();
-      let FPS = (totalCounts / (currentTime - startTime)) * 1000; //time in miliseconds
-      console.log("FPS", FPS);
-      exponentialSmoothing = Math.min(0.01 * FPS + 0.16, 0.9);
+
     }
-    rafID = requestAnimationFrame(frameLandmarks);
-  }
-  frameLandmarks();
-};
-
-async function main() {
-  // await tf.setBackend("webgl");
-
-  // model = await faceLandmarksDetection.load(
-  //   faceLandmarksDetection.SupportedPackages.mediapipeFacemesh,
-  //   { shouldLoadIrisModel: false, maxFaces: 1 }
-  // );
-
-  ////tfjs
-  // const model = poseDetection.SupportedModels.BlazePose;
-
-  // console.log("model", model);
-
-  // const detectorConfig = {
-  //   runtime: "tfjs",
-  //   enableSmoothing: true,
-  //   modelType: "full", //heavy
-  // };
-  // detector = await poseDetection.createDetector(model, detectorConfig);
-
-  // console.log("detector", detector);
-
-  // //mediapipe
-  // const model = poseDetection.SupportedModels.BlazePose;
-
-  // console.log("model", model);
-
-  // const detectorConfig = {
-  //   runtime: "mediapipe",
-  //   solutionPath: "base/node_modules/@mediapipe/pose",
-  // };
-  // detector = await poseDetection.createDetector(model, detectorConfig);
-
-  // console.log("detector", detector);
-
-  // let video;
-
-  // MoveNet
-  const detectorConfig = {
-    modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING,
-  };
-  const detector = await poseDetection.createDetector(
-    poseDetection.SupportedModels.MoveNet,
-    detectorConfig
-  );
-
-  // // PoseNet
-  // const detectorConfig = {
-  //   architecture: "ResNet50",
-  //   outputStride: 16,
-  //   inputResolution: { width: 257, height: 200 },
-  //   quantBytes: 4,
-  // };
-
-  // const detector = await poseDetection.createDetector(
-  //   poseDetection.SupportedModels.PoseNet,
-  //   detectorConfig
-  // );
-
-  // const detectorConfig = {
-  //   architecture: "MobileNetV1",
-  //   outputStride: 16,
-  //   inputResolution: { width: 640, height: 480 },
-  //   multiplier: 0.75,
-  // };
-  // const detector = await poseDetection.createDetector(
-  //   poseDetection.SupportedModels.PoseNet,
-  //   detectorConfig
-  // );
-
-  try {
-    video = await loadVideo();
-  } catch (e) {
-    console.log(e.message);
-    throw e;
-  }
-
-  canvasElement.width = video.videoWidth;
-  canvasElement.height = video.videoHeight;
-
-  startTime = Date.now();
-  landmarksRealTime(video, detector);
-}
-
-navigator.getUserMedia =
-  navigator.getUserMedia ||
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia;
-
-main();
+     rafID = requestAnimationFrame(frameLandmarks);
+   };
+ 
+   frameLandmarks();
+ };
+ 
+ navigator.getUserMedia = navigator.getUserMedia ||
+     navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+ 
+ main();
